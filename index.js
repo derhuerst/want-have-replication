27c5a865d550b62db45838a6392fbbd074a50a28
Jannis R <mail@jannisr.de>
2017-10-06 07:07:24 +0200

    WIP

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const {Readable, Writable} = require('stream')
 const duplexer = require('duplexer3')
 
 const HANDSHAKE = 0 // to find the leader
-const HAVE = 1 // has an item
-const WANT = 2 // wants an item
-const SEND = 3 // is going to send an item
+const HAVE = 1 // peer has an item
+const WANT = 2 // peer wants an item
+const RECEIVE = 3 // leader is going to send an item
+const SEND = 4 // leader is going to wait for an item
 
 const createPeer = (name, onItem) => { // todo: remove name
 	const have = Object.create(null)
@@ -21,10 +22,23 @@ const createPeer = (name, onItem) => { // todo: remove name
 	}
 
 	const replicate = () => {
-		const peerWants = []
 		let handshakeDone = false, isLeader, x
 
+		const peerHas = []
+		const peerWants = []
+		let receivingId = null
+
 		const onPkt = (pkt, _, cb) => { // [command, optional payload]
+			if (receivingId) {
+				have[receivingId] = pkt
+				peer.emit('_have', receivingId)
+				receivingId = null
+
+				setImmediate(onItem, pkt)
+				if (isLeader) setTimeout(tick)
+				return cb()
+			}
+
 			if (!Array.isArray(pkt) || pkt.length === 0) {
 				return cb(new Error('invalid packet format'))
 			}
@@ -41,27 +55,33 @@ const createPeer = (name, onItem) => { // todo: remove name
 				}
 				isLeader = peerX < x
 				handshakeDone = true
-				console.error(name, 'leader?', isLeader)
 			} else {
-				const id = pkg[1]
+				const id = pkt[1]
 				if ('string' !== typeof id) return cb() // invalid data
 
 				if (cmd === HAVE) {
+					if (!peerHas.includes(id)) peerHas.push(id)
+					const i = peerWants.indexOf(id)
+					if (i >= 0) peerWants.splice(i, 1)
+
 					if (!(id in have) && !want.includes(id)) {
 						want.push(id)
 						peer.emit('_want', id)
 					}
 				} else if (cmd === WANT) {
 					if (!peerWants.includes(id)) peerWants.push(id)
+				} else if (cmd === RECEIVE) {
+					if (isLeader) return cb() // invalid command
+					receivingId = id
 				} else if (cmd === SEND) {
 					if (isLeader) return cb() // invalid command
-					// todo
+					outgoing.push(have[id])
 				} else {
-					console.error(name, 'unknown packet', pkt)
 					return cb()
 				}
 			}
-			if (isLeader) setTimeout(tick)
+
+			if (isLeader && handshakeDone) setTimeout(tick)
 			return cb()
 		}
 
@@ -78,11 +98,41 @@ const createPeer = (name, onItem) => { // todo: remove name
 			x = Math.random()
 			outgoing.push([HANDSHAKE, x])
 		}
-		sendHandshake()
 
 		const tick = () => {
-			// todo
+
+			let i = want.findIndex(id => peerHas.includes(id))
+			if (i >= 0) {
+				receivingId = want[i]
+				want.splice(i, 1)
+				outgoing.push([SEND, receivingId])
+				return
+			}
+
+			i = peerWants.findIndex(id => have[id])
+			if (i >= 0) {
+				const id = peerWants[i]
+				peerWants.splice(i, 1)
+				outgoing.push([RECEIVE, id])
+				outgoing.push(have[id])
+
+				setTimeout(tick)
+			} else {
+				// todo: done events
+			}
 		}
+
+		for (let id in have) {
+			peerWants.push(id)
+			outgoing.push([HAVE, id])
+		}
+		for (let id of want) outgoing.push([WANT, id])
+		peer.on('_have', (id) => {
+			if (!peerHas.includes(id) && !peerWants.includes(id)) {
+				peerWants.push(id)
+			}
+		})
+		sendHandshake()
 
 		return duplexer({objectMode: true}, incoming, outgoing)
 	}
