a1ae49857009a06d559e1bdea29bceafe4ef081a
Jannis R <mail@jannisr.de>
2019-01-04 18:51:19 +0800

    tests: adapt to rewrite, debounce for robustness :white_check_mark:

--- test.js
+++ test.js
@@ -2,18 +2,19 @@
 
 const test = require('tape')
 const sortBy = require('lodash.sortby')
+const debounce = require('lodash.debounce')
 
 const createPeer = require('.')
 
 const noop = () => {}
 
 test('A <-> B replication', (t) => {
-	t.plan(2 + 2 + 2)
+	t.plan(2 + 2)
 
 	const i1 = ['bar', 'baz']
-	const onI2 = item => t.deepEqual(item, i2)
+	const onI2 = (_, item) => t.deepEqual(item, i2)
 	const i2 = ['foo', 'bar']
-	const onI1 = item => t.deepEqual(item, i1)
+	const onI1 = (_, item) => t.deepEqual(item, i1)
 
 	const p1 = createPeer('p1', onI2)
 	p1.add(i1)
@@ -27,16 +28,16 @@ test('A <-> B replication', (t) => {
 	const r2 = p2.replicate()
 	r1.pipe(r2).pipe(r1)
 
-	r1.on('synced', () => {
-		t.deepEqual(sortBy(p1.all(), 0), [i1, i2])
-	})
-	r2.on('synced', () => {
+	r1.on('synced', debounce(() => {
 		t.deepEqual(sortBy(p2.all(), 0), [i1, i2])
-	})
-})
+	}, 100))
+	r2.on('synced', debounce(() => {
+		t.deepEqual(sortBy(p1.all(), 0), [i1, i2])
+	}, 100))
+}, {timeout: 1000})
 
 test('A <-> B <-> C replication', (t) => {
-	t.plan(4)
+	t.plan(3)
 
 	const first = ['first item']
 	const A = createPeer('A', noop)
@@ -51,14 +52,18 @@ test('A <-> B <-> C replication', (t) => {
 	C.add(third)
 
 	const rA = A.replicate()
-	rA.pipe(B.replicate()).pipe(rA)
+	const rB = B.replicate()
+	rA.pipe(rB).pipe(rA)
 	const rC = C.replicate()
 	rC.pipe(B.replicate()).pipe(rC)
 
-	rA.on('synced', () => {
+	rA.on('synced', debounce(() => {
+		t.deepEqual(sortBy(B.all(), 0), [first, second, third])
+	}, 100))
+	rB.on('synced', debounce(() => {
 		t.deepEqual(sortBy(A.all(), 0), [first, second, third])
-	})
-	rC.on('synced', () => {
-		t.deepEqual(sortBy(C.all(), 0), [first, second, third])
-	})
-})
+	}, 100))
+	rC.on('synced', debounce(() => {
+		t.deepEqual(sortBy(B.all(), 0), [first, second, third])
+	}, 100))
+}, {timeout: 1000})
